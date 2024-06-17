2cbba83578ec981ff85fb3599ab4b7af2f9a908d
fernanchankleta <fernando.carrasco.tobar@gmail.com>
2024-06-17 12:53:10 -0400

    endpoints de las instalaciones

--- backend/routes/instalacion.js
+++ backend/routes/instalacion.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = require('express');
+const router  = express.Router();
 const {
     createInstalacion,
     updateInstalacion,
@@ -13,24 +13,84 @@ const {
 router.post("/createinstalacion", async (req, res) =>{
     
     try {
-        const bcryptPassword = await bcrypt.hash(password, 10);
-        const userAttributes = {
-            username: req.body.username,
-            password: bcryptPassword,
-            email: req.body.email,
-            profile: req.body.profile
+        const instalacionAttributes = {
+            tipo: req.body.tipo,
+            ubicacion: req.body.ubicacion,
+            fotografia: req.body.fotografia,
+            disponibilidad: req.body.disponibilidad
         }
-        const user = await createUser(userAttributes);
+        const instalacion = await createInstalacion(instalacionAttributes);
 
-        if (!user) {
-            return res.statusCode(500).json({message: "No se pudo registrar el usuario"});
+        if (!instalacion) {
+            return res.statusCode(500).json({message: "No se pudo registrar la instalacion"});
         }
 
-        return res.json({message: "Usuario creado exitosamente"});
+        return res.json({message: "Instalacion creada exitosamente"});
     } catch (error) {
         return res.status(500).json({ message: 'Error interno del servidor' });
     }
+});
 
+router.patch("/updateinstalacion", async (req, res) => {
 
+    try {
+        const instalacionAttributes = {
+            tipo: req.body.tipo,
+            ubicacion: req.body.ubicacion,
+            fotografia: req.body.fotografia,
+            disponibilidad: req.body.disponibilidad
+        }
+        const instalacion = await updateInstalacion(req.body.id, instalacionAttributes);
+
+        if (!instalacion) {
+            return res.statusCode(500).json({message: "No se pudo actualizar la instalacion"});
+        }
+
+        return res.json({message: "Instalacion actualizada exitosamente"});
+    } catch (error) { 
+        return res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
+router.delete("/deleteinstalacion", async (req, res) => {
+    
+    try {
+        
+        const instalacion = await deleteInstalacion(req.body.id);
+
+        if (!instalacion) {
+            return res.statusCode(500).json({message: "No se pudo eliminar la instalacion"});
+        }
+
+        return res.json({message: "Instalacion eliminada exitosamente"});
+    } catch (error) { 
+        return res.status(500).json({ message: 'Error interno del servidor' });
+    }
 
 });
+
+router.get("/getuser", async (req, res) => {
+
+    try {
+        
+        const user = await getInstalacionById(req.body.id);
+        
+
+        if (!user) {
+            return res.statusCode(500).json({message: "No se pudo encontrar la instalacion"});
+        }
+
+        return res.json(user);
+    } catch (error) { 
+        return res.status(500).json({ message: 'Error interno del servidor' });
+    }
+
+});
+
+
+
+
+
+
+module.exports = router;
+
